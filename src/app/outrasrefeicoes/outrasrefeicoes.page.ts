9f0630098a728d3a6788bceb91809dd50980420c
Luiz <luizfelipebaracat97@gmail.com>
2021-06-13 20:22:47 -0300

    Projeto Final - APS - Desenvolvimento Mobile - SI - UAM Mocca

--- src/app/outrasrefeicoes/outrasrefeicoes.page.ts
+++ src/app/outrasrefeicoes/outrasrefeicoes.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-outrasrefeicoes',
@@ -7,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutrasrefeicoesPage implements OnInit {
 
-  constructor() { }
+  constructor(public alertController: AlertController) { }
+
+  public AlimentosIngeridos: String;
+  public AguaTomada: String;
+  public CaloriasIngeridas: String;
+  public HorariodaRefeicao: String;
+
+  async showAlertSucesso() {
+    const alert = await this.alertController.create({
+      header: 'Almoço Enviado',
+      message: 'Seu Almoço foi enviado para sua nutricionista',
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  enviarJantar(){
+    this.showAlertSucesso();
+    this.CaloriasIngeridas = "";
+    this.AguaTomada = "";
+    this.AlimentosIngeridos = "";
+    this.HorariodaRefeicao = "";
+  }
 
   ngOnInit() {
   }
